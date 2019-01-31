538329366b3852335f8afffd2689178a0a688d56
thesubtlety <noah@thesubtlety.com>
2019-01-30 17:10:26 -0800

    yolo commit

--- install.sh
+++ install.sh
@@ -15,73 +15,65 @@ RED_ELK_SERVER_IP="10.10.10.10"
 declare -a teamserver_list=("10.0.10.200") #space separated list e.g. ("1.2.3.4" "4.3.2.1")
 declare -a redir_list=("10.0.10.100")
 
-fixup_locals() {
-	# update timezones
-	find ./ -type f -exec sed -i 's/Europe\/Amsterdam/UTC/g' {} \;
+#Fix up locals
+# update timezones
+find ./ -type f -exec sed -i 's/Europe\/Amsterdam/UTC/g' {} \;
 
-	#update cert info
-	sed -i 's/C\ =\ MODIFYME.*$/C\ =\ US' certs/config.cnf
-	sed -i 's/MODIFYME.*$/TrustMe/g' certs/config.cnf
-	sed -i "s/dnsnameofyourredelkserver/$RED_ELK_SERVER/" certs/config.cnf
-	sed -i "s/someseconddnsname/$RED_ELK_SERVER/" certs/config.cnf
-	sed -i "s/123\.123\.123\.123/$RED_ELK_SERVER_IP/" certs/config.cnf
+#update cert info
+sed -i 's/C\ =\ MODIFYME.*$/C\ =\ US' certs/config.cnf
+sed -i 's/MODIFYME.*$/TrustMe/g' certs/config.cnf
+sed -i "s/dnsnameofyourredelkserver/$RED_ELK_SERVER/" certs/config.cnf
+sed -i "s/someseconddnsname/$RED_ELK_SERVER/" certs/config.cnf
+sed -i "s/123\.123\.123\.123/$RED_ELK_SERVER_IP/" certs/config.cnf
 
-	# Cobalt strike in a non standard location, fix scripts
-	find ./ -type f -exec sed -i -e 's/\/root\/cobaltstrike/\/opt\/cobaltstrike/g' {} \;
+# Cobalt strike in a non standard location, fix scripts
+find ./ -type f -exec sed -i -e 's/\/root\/cobaltstrike/\/opt\/cobaltstrike/g' {} \;
 
-	# Initial Setup
-	sudo ./initial-setup.sh
-}
+# Initial Setup
+sudo ./initial-setup.sh
 
-configure_redirectors() {
-	# Redirectors equire haproxy installed and running
-	# replace redirector, scenario with whatever you want displayed in Kibana #TODO
-	for REDIR_IP in "${redir_list[@]}"; do
-		scp -i $PEM redirs.tgz ec2-user@$REDIR_IP:~
-		ssh -i $PEM ec2-user@$REDIR_IP <<EOF
+
+# Configure redirectors
+# Redirectors equire haproxy installed and running
+# replace redirector, scenario with whatever you want displayed in Kibana #TODO
+for REDIR_IP in "${redir_list[@]}"; do
+	scp -i $PEM redirs.tgz ec2-user@$REDIR_IP:~
+	ssh -i $PEM ec2-user@$REDIR_IP <<EOF
 tar xvf redirs.tgz
 cd redirs
 sudo ./install-redir.sh Redirector Scenario $RED_ELK_SERVER_IP:5044
 EOF
 done
-}
 
-configure_teamservers() {
-	for TEAMSERVER_IP in "${teamserver_list[@]}"; do
-		scp -i $PEM teamservers.tgz ec2-user@$TEAMSERVER_IP:~
-		ssh -i $PEM ec2-user@$TEAMSERVER_IP <<EOF
+
+# Configure Team Servers
+for TEAMSERVER_IP in "${teamserver_list[@]}"; do
+	scp -i $PEM teamservers.tgz ec2-user@$TEAMSERVER_IP:~
+	ssh -i $PEM ec2-user@$TEAMSERVER_IP <<EOF
 tar xvf teamservers.tgz
 cd teamservers
 sudo ./install-teamserver.sh TeamServer Scenario $RED_ELK_SERVER_IP:5044
 EOF
 done
-}
 
-configure_elkserver() {
-	scp -i $PEM elkserver.tgz ec2-user@$RED_ELK_SERVER_IP:~
-	ssh -i $PEM ec2-user@$RED_ELK_SERVER_IP <<EOF
+
+# Configure Elk Server
+scp -i $PEM elkserver.tgz ec2-user@$RED_ELK_SERVER_IP:~
+ssh -i $PEM ec2-user@$RED_ELK_SERVER_IP <<EOF
 tar xvf elkserver.tgz
 cd elkserver
 sudo ./install-elkserver.sh
 EOF
-	for TEAMSERVER_IP in "${teamserver_list[@]}"; do
-		ssh -i $PEM ec2-user@$RED_ELK_SERVER_IP <<EOF
+for TEAMSERVER_IP in "${teamserver_list[@]}"; do
+	ssh -i $PEM ec2-user@$RED_ELK_SERVER_IP <<EOF
 sudo echo "#*/2 * * * * redelk /usr/share/redelk/bin/getremotelogs.sh $TEAMSERVER_IP TeamServer scponly" >> /etc/cron.d/redelk #s/kali/tsdisplayname/
 EOF
-	done
+done
 
-	echo "Update scripts in /etc/redelk/* as needed."
-	echo "If things aren't working, check the following logs on the Elk server"
-	echo "/var/log/filebeat/filebeat"
-	echo "/var/log/filestash/filestash-plain.log"
-	echo
-	echo "Done"
-}
-
-
-fixup_locals()
-configure_redirectors()
-configure_teamservers()
-configure_elkserver()
-
+echo "Update scripts in /etc/redelk/* as needed."
+echo "If things aren't working, check the following logs on the Elk server"
+echo "/var/log/filebeat/filebeat"
+echo "/var/log/filestash/filestash-plain.log"
+echo
+echo "Done"
 
